8614b5523e7138b6ab552c8fbc11d45d97c7420f
SandipNITA2025 <sarkarsandip7910@gmail.com>
2023-12-13 12:32:16 +0530

    using react query

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,137 @@
-// import  from 'react'
+import { useState, useEffect } from "react";
+import axios from "axios";
 import { CiSearch } from "react-icons/ci";
-import { ChevronDown, Heart, Minus } from "lucide-react";
+import { ChevronDown, Heart } from "lucide-react";
 import { IoPlay } from "react-icons/io5";
 import { IoPlaySkipForward, IoPlaySkipBack } from "react-icons/io5";
 import { RxLoop } from "react-icons/rx";
 import { MdFormatListBulleted } from "react-icons/md";
 import { HiPause } from "react-icons/hi2";
-import { useState } from "react";
 import { RxCross2 } from "react-icons/rx";
+import { useQuery } from "react-query";
 
-const musicList = () => (
-  <div className=" border-t mt-1 rounded-lg p-1 flex gap-2">
-    <div className="left">
-      <img
-        className="w-[38px]  h-[38px] rounded-md object-cover"
-        src="https://i.postimg.cc/FR7mD4Lf/Adobe-Stock-194945297-Preview.jpg"
-        alt=""
-      />
-    </div>
-    <div className="right">
-      <h3 className="max-w-[80%] font-medium text-[14px] h-[20px] overflow-hidden">
-        {" "}
-        Lorem ipsum sjhdb dhd dhc dd d dd dd
-      </h3>
-      <p className=" text-[12px]">Singer: Arijit Singh</p>
-    </div>
-  </div>
-);
+const fetchPlaylist = async () => {
+  const response = await axios.get(
+    "https://spotify23.p.rapidapi.com/playlist_tracks/",
+    {
+      params: {
+        id: "37i9dQZF1DX4Wsb4d7NKfP",
+        offset: "0",
+        limit: "20",
+      },
+      headers: {
+        "X-RapidAPI-Key": import.meta.env.VITE_X_API_KEY,
+        "X-RapidAPI-Host": "spotify23.p.rapidapi.com",
+      },
+    }
+  );
+
+  return response.data?.items || [];
+};
 
 const App = () => {
   const [isModelOpen, setIsModelOpen] = useState(false);
+  const [currentTrackIndex, setCurrentTrackIndex] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [audioRef, setAudioRef] = useState(null);
+  const [currentTime, setCurrentTime] = useState(0);
+  const [duration, setDuration] = useState(0);
+  const [isLiked, setIsLikes] = useState(false);
+
+  const { data: playlist = [], refetch } = useQuery("playlist", fetchPlaylist, {
+    refetchOnWindowFocus: false,
+  });
+
+  useEffect(() => {
+    refetch();
+  }, [refetch]);
+
+  // console.log(playlist);
+
+  useEffect(() => {
+    if (audioRef) {
+      audioRef.addEventListener("timeupdate", handleTimeUpdate);
+      audioRef.addEventListener("loadedmetadata", handleDurationChange);
+
+      return () => {
+        audioRef.removeEventListener("timeupdate", handleTimeUpdate);
+        audioRef.removeEventListener("loadedmetadata", handleDurationChange);
+      };
+    }
+  }, [audioRef]);
+
+  const handleTimeUpdate = () => {
+    setCurrentTime(audioRef.currentTime);
+  };
+
+  const handleDurationChange = () => {
+    setDuration(audioRef.duration);
+  };
+
+  const playPauseHandler = () => {
+    if (audioRef) {
+      if (isPlaying) {
+        audioRef.pause();
+      } else {
+        audioRef.play();
+      }
+      setIsPlaying(!isPlaying);
+    }
+  };
+
+  const nextSongHandler = () => {
+    setCurrentTrackIndex((prevIndex) => (prevIndex + 1) % playlist.length);
+    setIsPlaying(!isPlaying);
+  };
+
+  const prevSongHandler = () => {
+    setCurrentTrackIndex(
+      (prevIndex) => (prevIndex - 1 + playlist.length) % playlist.length
+    );
+    setIsPlaying(!isPlaying);
+  };
+
+  const selectTrackHandler = (index) => {
+    setCurrentTrackIndex(index);
+    setIsPlaying(false);
+  };
+
+  const handleAudioEnded = () => {
+    // Automatically play the next song when the current one ends
+    nextSongHandler();
+    setIsPlaying(!isPlaying);
+  };
+
+  const handleSliderChange = (event) => {
+    if (audioRef) {
+      const newTime = event.target.value;
+      setCurrentTime(newTime);
+      audioRef.currentTime = newTime;
+    }
+  };
+
+  const formatTime = (timeInSeconds) => {
+    const minutes = Math.floor(timeInSeconds / 60);
+    const seconds = Math.floor(timeInSeconds % 60);
+    return `${String(minutes).padStart(2, "0")}:${String(seconds).padStart(
+      2,
+      "0"
+    )}`;
+  };
 
   const handleOpenLists = () => {
     setIsModelOpen(!isModelOpen);
   };
 
+  const handleLike = () => {
+    setIsLikes(!isLiked);
+  };
+
+  // console.log(playlist[0].track?.album?.images[0]?.url);
+
   return (
     <div
-      className={`bg-white  w-[100vw h-[100vh] overflow-hidden sm:flex sm:items-center sm:justify-center p-[1.1rem] relative ${
+      className={`bg-gray-100  w-[100vw] h-[100vh] overflow-hidden sm:flex sm:items-center sm:justify-center p-[1.2rem] relative ${
         isModelOpen ? "open-model" : "closed-model"
       }`}
     >
@@ -57,7 +151,7 @@ const App = () => {
         <div className="mt-8 w-[100%] h-[320px]  rounded-xl overflow-hidden">
           <img
             className="w-[100%] h-full object-cover"
-            src="https://i.postimg.cc/FR7mD4Lf/Adobe-Stock-194945297-Preview.jpg"
+            src={playlist[currentTrackIndex]?.track?.album?.images[0]?.url}
             alt=""
           />
         </div>
@@ -65,27 +159,61 @@ const App = () => {
         <div className="titlebox w-[98%] mt-8 flex items-start flex-col justify-center">
           <div className=" flex items-center w-[98%] justify-between">
             <p className=" max-w-[80%] font-medium h-[26px] overflow-hidden">
-              {" "}
-              Lorem ipsum sjhdb dhd dhc dd d dd dd{" "}
+              {playlist[currentTrackIndex]?.track?.name}
             </p>
-            <Heart size={20} />
+            <Heart
+              onClick={handleLike}
+              color={`${isLiked ? "red" : "black"}`}
+              size={20}
+            />
           </div>
-          <p className=" text-[12px]">Singer: Arijit Singh</p>
+          <p className=" text-[12px]">
+            Artist:{" "}
+            {playlist[currentTrackIndex]?.track?.album?.artists[0]?.name}
+          </p>
         </div>
 
         <div className="mt-8 w-[98%] flex items-start flex-col justify-center">
-          <input className="rounded-lg  h-1 w-[98%]" type="range" />
+          <input
+            type="range"
+            min="0"
+            max={duration}
+            step="1"
+            value={currentTime}
+            onChange={handleSliderChange}
+          />
           <div className=" flex w-[100%] mt-1 items-center justify-between">
-            <p className="starttime text-[13px]">0:0</p>
-            <p className="endtime text-[13px]">6:10</p>
+            <p className="starttime text-[13px]">{formatTime(currentTime)}</p>
+            <p className="endtime text-[13px]">{formatTime(duration)}</p>
           </div>
         </div>
-
-        <div className="w-[98%] flex items-center justify-between mt-8">
+        <audio
+          ref={(audio) => setAudioRef(audio)}
+          src={playlist[currentTrackIndex]?.track?.preview_url}
+          onEnded={handleAudioEnded}
+        />
+        <div className="w-[98%] flex items-center justify-between mt-8 pb-3">
           <RxLoop className=" active:scale-90" />
-          <IoPlaySkipBack className=" active:scale-90" />
-          <IoPlay className="active:scale-90 shadow-lg w-[50px] h-[50px] p-2 rounded-full " />
-          <IoPlaySkipForward className=" active:scale-90" />
+          <IoPlaySkipBack
+            onClick={prevSongHandler}
+            className=" active:scale-90"
+          />
+          {isPlaying ? (
+            <HiPause
+              onClick={playPauseHandler}
+              className="active:scale-90 shadow-lg w-[50px] h-[50px] p-2 rounded-full "
+            />
+          ) : (
+            <IoPlay
+              onClick={playPauseHandler}
+              className="active:scale-90 shadow-lg w-[50px] h-[50px] p-2 rounded-full "
+            />
+          )}
+
+          <IoPlaySkipForward
+            onClick={nextSongHandler}
+            className=" active:scale-90"
+          />
           <MdFormatListBulleted
             onClick={handleOpenLists}
             className=" active:scale-90"
@@ -97,7 +225,7 @@ const App = () => {
         className={`model-content absolute bottom-0 bg-[#f5f6fa] w-[100%] left-0 rounded-lg px-4 overflow-y-auto transition-transform duration-300 transform ${
           isModelOpen ? "translate-y-0" : "translate-y-full"
         }`}
-        style={{ height: "400px" }}
+        style={{ height: "500px" }}
       >
         <div className="  mt-2 w-full flex items-center justify-center">
           <ChevronDown
@@ -106,22 +234,37 @@ const App = () => {
           />
         </div>
 
+        {playlist?.map((item, index) => {
+          // console.log(item);
+          return (
+            <div
+              key={index}
+              className=" border-t mt-1 rounded-lg p-1 flex gap-2"
+              onClick={() => {
+                selectTrackHandler(index), handleOpenLists();
+              }}
+            >
+              <div className="left">
+                <img
+                  className="w-[38px]  h-[38px] rounded-md object-cover"
+                  src={item?.track?.album?.images[2]?.url}
+                  alt=""
+                />
+              </div>
+              <div className="right">
+                <h3 className=" title max-w-[80%] font-medium text-[14px] h-[20px] overflow-hidden">
+                  {" "}
+                  {item?.track?.name}
+                </h3>
+                <p className="singer text-[12px]">
+                  Artist: {item?.track?.album?.artists[0]?.name}
+                </p>
+              </div>
+            </div>
+          );
+        })}
+
         {/* Your model content goes here */}
-        {musicList()}
-        {musicList()}
-        {musicList()}
-        {musicList()}
-        {musicList()}
-        {musicList()}
-        {musicList()}
-        {musicList()}
-        {musicList()}
-        {musicList()}
-        {musicList()}
-        {musicList()}
-        {musicList()}
-        {musicList()}
-        {musicList()}
       </div>
     </div>
   );
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,34 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import {VitePWA} from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: "Musicfy",
+        short_name: "Musicfy",
+        description:
+          "Musicfy",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#fff",
+        icons: [
+          {
+            src: "/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/android-chrome-384x384.png",
+            sizes: "384x384",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
